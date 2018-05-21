7fd77d5d72d0c3c74072f53aaaf820088a4c6237
AreaScout <d.mehrwald@gmail.com>
2018-05-21 10:12:35 +0200

    Add: Ring Twister shader

---
+++ shaders/ringtwister.f.glsl
@@ -0,0 +1,28 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform vec2 resolution;
+uniform float time;
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+
+void main()
+{
+	vec2 fragCoord;
+	vec4 fragColor;
+    float
+        T = time,
+        r = length(fragCoord += gl_FragCoord.xy - (fragColor.xy = (resolution.xy/2.))) / fragColor.y * 4. - 2.8,
+        a = atan(fragCoord.y, fragCoord.x),
+        B = mod( a+T + sin(a-=T) * sin(T) * 3.14, 1.57 )  - 2.;
+    
+    r -= T = sin( B += r > cos(B) ? 1.6 : 0.);
+    B = cos(B)- T;   
+
+    fragColor =  vec4(5, 9, 9, 0)*B - vec4(-5, 3, 9, 0) * r;
+    fragColor *= smoothstep(.45, .4, T = abs(r/B-.5) ) + .5;
+  
+    fragColor *= T<.5 ? sin(r/B * 13.) * cos(a * 16.) < 0. ? .04 : .05 : 0.;
+	gl_FragColor = fragColor;
+}
